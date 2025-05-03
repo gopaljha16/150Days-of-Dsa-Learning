8ec2916f65a586c799bbc89a0bdd9cbf8f7fabc5
Gopal Jha <gopaljha9398715741@gmail.com>
2025-05-04 00:22:03 +0530

    File Handling in C++ + homework done

--- Day 51/Homework.cpp
+++ Day 51/Homework.cpp
@@ -4,15 +4,28 @@
 #include <vector>
 using namespace std;
 
-int main(){
+int main()
+{
     ifstream fin;
     fin.open("test.txt");
-    
-     vector<int>ans(5);
 
-     ofstream fout;
-     fout.open("")
-     while(!fin.eof()){
-        
-     }
+    vector<int> arr;
+    int x;
+
+    while (fin >> x)
+    {
+        arr.push_back(x);
+    }
+
+    fin.close();
+    sort(arr.begin(), arr.end());
+    ofstream fout("test.txt", ios::app); // append mode
+    fout << "\nSorted Array\n";
+
+    for (int i = 0; i < 5; i++)
+    {
+        fout << arr[i] << " ";
+    }
+
+    fout.close();
 }
